ab41cb85aedc0cbb8ad5c885a54470ef5a530327
Prateek Jha <prateekj777@gmail.com>
2020-07-04 19:24:47 +0530

    initailaizing new git repo

---
+++ precache-manifest.14ef3b029c59a1548e6a8d521d2ee41a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad973a6f1ae1ac711c396cf09e8b8d48",
+    "url": "/jha-prateek.github.io/index.html"
+  },
+  {
+    "revision": "05b19f4db0b7b3b52a82",
+    "url": "/jha-prateek.github.io/static/css/main.8881d2cc.chunk.css"
+  },
+  {
+    "revision": "f30d58cd1c7dd6d339ba",
+    "url": "/jha-prateek.github.io/static/js/2.cc738766.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jha-prateek.github.io/static/js/2.cc738766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b19f4db0b7b3b52a82",
+    "url": "/jha-prateek.github.io/static/js/main.1a4682ef.chunk.js"
+  },
+  {
+    "revision": "fcb9e3f3e656d72dd4e5",
+    "url": "/jha-prateek.github.io/static/js/runtime-main.e38c3c7b.js"
+  },
+  {
+    "revision": "29cb1ca33fcfd779ed621e9328b4beb1",
+    "url": "/jha-prateek.github.io/static/media/profile.29cb1ca3.png"
+  }
+]);
