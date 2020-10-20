d0022f6f672dbd851a7a0bae1fccb1625a1a8a56
Prateek Jha <prateekj777@gmail.com>
2020-10-20 23:09:07 +0530

    Deploy [10-20-20 23:09:07]

---
+++ precache-manifest.fdfe57cec54144e2d3a1ae98e705b275.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f06196b6b5bfb4603367c25d2fdcb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6e12ec0686002da2ce2",
+    "url": "/static/css/2.13796217.chunk.css"
+  },
+  {
+    "revision": "b024a3f0e68701b8ea5d",
+    "url": "/static/css/main.134daeaa.chunk.css"
+  },
+  {
+    "revision": "d6e12ec0686002da2ce2",
+    "url": "/static/js/2.d9d2d34a.chunk.js"
+  },
+  {
+    "revision": "5e170eb872bc712569a704d2fb00e2e6",
+    "url": "/static/js/2.d9d2d34a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b024a3f0e68701b8ea5d",
+    "url": "/static/js/main.c30d736d.chunk.js"
+  },
+  {
+    "revision": "5e4973422a2125bf5c19",
+    "url": "/static/js/runtime-main.7e3706cb.js"
+  },
+  {
+    "revision": "29cb1ca33fcfd779ed621e9328b4beb1",
+    "url": "/static/media/profile.29cb1ca3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jha-prateek.github.io/precache-manifest.fb1d220fea14fea853d87f7fc5788beb.js"
+  "/precache-manifest.fdfe57cec54144e2d3a1ae98e705b275.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jha-prateek.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
